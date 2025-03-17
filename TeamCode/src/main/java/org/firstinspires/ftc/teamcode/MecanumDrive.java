384174a1ee3c56dc5d557d0bc33521fc24f38e5b
William Fleming <william.fleming@ums.legacypca.org>
2025-03-17 12:06:41 -0500

    started tuning, values should be correct

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -62,13 +62,13 @@ public final class MecanumDrive {
                 RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
 
         // drive model parameters
-        public double inPerTick = 1;
+        public double inPerTick = 0.00297999668;
         public double lateralInPerTick = inPerTick;
         public double trackWidthTicks = 0;
 
         // feedforward parameters (in tick units)
-        public double kS = 0;
-        public double kV = 0;
+        public double kS = 1.1163432468542114;
+        public double kV = 0.0005635617239438279;
         public double kA = 0;
 
         // path profile parameters (in inches)
